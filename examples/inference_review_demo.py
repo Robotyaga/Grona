1a5def11c7a0af82dd7cf0d409cbd9e13c03bac3
Robotyaga <46932112+Robotyaga@users.noreply.github.com>
2026-06-06 10:11:35 +0300

    Add inference review demo example

---
+++ examples/inference_review_demo.py
@@ -0,0 +1,7 @@
+"""Run the deterministic inference review demo."""
+
+from grona.inference_review_cli import format_inference_review_demo
+
+
+if __name__ == "__main__":
+    print(format_inference_review_demo())
